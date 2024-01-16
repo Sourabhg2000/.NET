b2dc7f5c3a320c16d3c54d57dbbfa3ccf9fba2cf
Sourabh Gaikwad <sourabhg9009@gmail.com>
2024-01-16 23:28:09 +0530

    Console App

---
+++ InterviewApp/InterviewApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Infoway Interview Application!");
+            try
+            {
+                Student student = new Student();
+                student.Selected += PassedStudents;
+                student.Selected += ExcellentStudents;
+                student.Rejected += FailedStudents;
+                student.StudentId = 34892;
+                student.StudentName = "Test";
+                student.City = "Test";
+                student.TotalMarks = 57;
+                if (student.TotalMarks<95)
+                {
+                    student.Selected -= ExcellentStudents;
+                }
+                bool confirm=false;
+                string result = student.CalculateResult(student.TotalMarks,ref confirm);
+                Console.WriteLine(result);
+                Console.WriteLine(confirm);
+                student.InterviewRounds("Round-1");
+                student.InterviewRounds("Round-1","Round-2");
+                student.InterviewRounds("Round-1", "Round-2", "Round-3","Round-4");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+        private static void PassedStudents()
+        {
+            Console.WriteLine("Record stored in Oracle Database!");
+        }
+        private static void ExcellentStudents()
+        {
+            Console.WriteLine("Record stored in DB2 Database!");
+        }
+        private static void FailedStudents()
+        {
+            Console.WriteLine("Record stored in MySQL Server Database!");
+        }
+    }
+    
+}
---
+++ InterviewApp/InterviewApp/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewApp
+{
+    public delegate void InfowayDelegate();
+    internal class Student
+    {
+        private int m_StudentId;
+        public event InfowayDelegate Selected;
+        public event InfowayDelegate Rejected;
+        public int StudentId
+        {
+            get
+            {
+                return m_StudentId;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Student Id Can not be zero or negative value!");
+                }
+                else
+                {
+                    m_StudentId = value;
+                }
+            }
+        }
+        public string StudentName { get; set; }
+        public string City { get; set; }
+        public int TotalMarks { get; set; }
+
+        //public string CalculateResult(int marks,out bool confirmation)
+        public string CalculateResult(int marks, ref bool confirmation)
+        {
+            if (marks > 80)
+            {
+                if (Selected != null)
+                {
+                    Selected();
+                }
+                confirmation = true;
+                return $"{StudentName} has been passed the interview!";
+            }
+            else
+            {
+                if (Rejected != null)
+                {
+                    Rejected();
+                }
+                //confirmation = false;
+                return $"{StudentName} is failed in the interview!";
+            }
+        }
+        public void InterviewRounds(params string[] rounds)
+        {
+            for (int i = 0; i < rounds.Length; i++)
+            {
+                Console.WriteLine(rounds[i]);
+            }
+        }
+    }
+}
