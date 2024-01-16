2b4bad7b7e4569100dfb8c50447298b6b99bff39
Sourabh Gaikwad <sourabhg9009@gmail.com>
2024-01-16 23:25:37 +0530

    Console App

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Please enter your name!");
+                string myName = Console.ReadLine();
+                Console.WriteLine("Please enter your age!");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Welcome To Infoway! C-DAC!");
+                Console.WriteLine("Welcome To DotNet Framework Training!");
+                Console.WriteLine($"My name is {myName} and My age is {age}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            Console.ReadKey();
+        }
+    }
+}
