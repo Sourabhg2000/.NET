47cbc3b5d8099325001e262f2d93c5aee8c25c3e
Sourabh Gaikwad <sourabhg9009@gmail.com>
2024-01-16 23:30:15 +0530

    CDAC

---
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IPhysics physics=new Science();
+            physics.PhysicsCalci();
+            IChemistry chemistry = new Science();
+            chemistry.ChemistryCalci();
+            IBiology biology = new Science();
+            biology.BiologyCalci();
+            Console.WriteLine("--------------------");
+            IScience science=new Science(); 
+            
+        }
+    }
+}
---
+++ InterfaceExample/InterfaceExample/Science.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    internal class Science : IScience
+    {
+        void IBiology.BiologyCalci()
+        {
+            Console.WriteLine("This is a Biology Department Calci!");
+        }
+        void IChemistry.ChemistryCalci()
+        {
+            Console.WriteLine("This is a Chemistry Department Calci!");
+        }
+        void IPhysics.PhysicsCalci()
+        {
+            Console.WriteLine("This is a Physics Department Calci!");
+        }
+    }
+}
